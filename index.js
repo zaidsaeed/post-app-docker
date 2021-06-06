26e63a48087a679aaa7a892f47ee9b9fd2442177
zaidsaeed <zsaee060@uottawa.ca>
2021-06-05 22:10:10 -0400

    added connection to database. Need to look into Schemas and routes next

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = process.env.PORT || 2000
 
+const connectionURL = 'mongodb://zaid:password@mongo:27017/?authSource=admin';
+
+const connectToDB = () => {
+  mongoose.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}).then(res => {console.log('Connection successful')}).catch(err => {
+    console.log(err)
+    setTimeout(connectToDB(), 5000);
+  });
+}
+
+connectToDB()
+
 app.get('/', (req, res) => {
-  res.send('Hello Zaid Saeed!!!!')
+  res.send('Hello Zaid Saeed!')
 })
 
 app.listen(port, () => {
